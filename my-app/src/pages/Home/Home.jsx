0f51b61c4224cbad2e7c45d8d904da59830d8fcf
amolkpatil22 <amolkpatil22@gmail.com>
2023-09-30 16:00:09 +0530

    day-4

--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react"
 import "./home.css"
 
 
+
+
 export const Home = () => {
     return (
         <Box >
-            <Box id="banner">
-                <Box marginLeft={"150px"} >
-                    <Heading color={"rgb(0,61,41)"} size={"4xl"} marginBottom={"20px"} >Shopping And</Heading>
+            <Box id="banner" >
+                <Box marginLeft={"150px"}  >
+                    <Heading  color={"rgb(0,61,41)"} size={"4xl"} marginBottom={"20px"} >Shopping And</Heading>
                     <Heading color={"rgb(0,61,41)"} size={"4xl"} marginBottom={"20px"}>Department Store</Heading>
                     <Text marginBottom={"30px"} marginTop={"40px"} fontSize={"xl"} width={"40%"}>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</Text>
                     <Button id="learnmore" size={"lg"} colorScheme="green" backgroundColor={"rgb(0,61,41)"} color={"white"} borderRadius={"30px"} marginBottom={"20px"}>Learn More</Button>
--- my-app/src/pages/Login/Login.jsx
+++ my-app/src/pages/Login/Login.jsx
@@ -1,7 +1,14 @@
+import { useStatStyles } from "@chakra-ui/react"
+import { useState } from "react"
 
 
-
+let intdata={
+    email:"",
+    password:""
+}
 
 export const Login=()=>{
-    
+
+
+
 }
